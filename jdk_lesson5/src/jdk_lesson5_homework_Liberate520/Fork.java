9244b072b2a9e9b6f06d8b1f971cf732e7585f02
julemalysheva <julemalysheva@gmail.com>
2023-11-26 10:45:19 +0500

    jdk_lesson5_homework_Liberate520

---
+++ jdk_lesson5/src/jdk_lesson5_homework_Liberate520/Fork.java
@@ -0,0 +1,13 @@
+package jdk_lesson5_homework_Liberate520;
+
+public class Fork {
+    private boolean using;
+
+    public boolean isUsing() {
+        return using;
+    }
+
+    public void setUsing(boolean using) {
+        this.using = using;
+    }
+}
---
+++ jdk_lesson5/src/jdk_lesson5_homework_Liberate520/Main.java
@@ -0,0 +1,10 @@
+package jdk_lesson5_homework_Liberate520;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Table table = new Table();
+        table.start();
+    }
+}
